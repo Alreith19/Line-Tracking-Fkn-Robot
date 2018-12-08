545ba8fdf018bd424ec423222a4c6bedcc9dfb17
Cedric <cedricholz@berkeley.edu>
2018-12-08 11:31:52 -0800

    added in sensor code

--- THE_CODE_THE_CODE/THE_CODE_THE_CODE.ino
+++ THE_CODE_THE_CODE/THE_CODE_THE_CODE.ino
@@ -1,56 +1,106 @@
 #include <AFMotor.h>
+#include <QTRSensors.h>
 
-// identifying positives & negatives of the motors. 
-AF_DCMotor motor4(4);
-AF_DCMotor left_motor(1);
+#define NUM_SENSORS   3
+#define TIMEOUT       2500
+#define EMITTER_PIN   2
+
+AF_DCMotor left_motor(4, MOTOR12_64KHZ);
+AF_DCMotor right_motor(1, MOTOR12_64KHZ);
+
+
+QTRSensorsRC qtrrc((unsigned char[]) {A0, A1, A2}, NUM_SENSORS, TIMEOUT, EMITTER_PIN);
+  
+unsigned int sensorValues[NUM_SENSORS];
+
 void setup()
-{  // sending voltage out to the motors
-  
-// telling arduino to read from these 
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+{ 
+  calibrate();
+  right_motor.setSpeed(255);
   left_motor.setSpeed(255);
-  motor4.setSpeed(255);
-  
-  
 }
 
 void loop()
-
 {
-  //Sensor 1
-  int right = analogRead(A0);
-  //Sensor 2
-  int center = analogRead(A1);
-  // Sensor 3
-  int left = analogRead(A2);
+  unsigned int position = qtrrc.readLine(sensorValues);
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
+  // 1000 means minimum reflectance, followed by the line position
+  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+
+  Serial.println(position);
+  int left;
+  int center;
+  int right;
   
-  
+  if (sizeof(sensorValues) > 0){
+    left = sensorValues[0];
+    center = sensorValues[1];
+    right = sensorValues[2];
 
   if (center > 600 )
   {
     // both wheels running
+   right_motor.run(FORWARD);
    left_motor.run(FORWARD);
-   motor4.run(FORWARD);
+   right_motor.setSpeed(255);
    left_motor.setSpeed(255);
-   motor4.setSpeed(255);
   }
   if (right >  600)
   {
     // only left is on. so going right
+    right_motor.run(FORWARD);
     left_motor.run(FORWARD);
-    motor4.run(FORWARD);
-    left_motor.setSpeed(255);
-    motor4.setSpeed(1);
+    right_motor.setSpeed(255);
+    left_motor.setSpeed(1);
   }
 
   if (left > 600 )
   {
     // right is on, so going left.
+    right_motor.run(FORWARD);
     left_motor.run(FORWARD);
-    motor4.run(FORWARD);
-    left_motor.setSpeed(1);
-    motor4.setSpeed(255);
+    right_motor.setSpeed(1);
+    left_motor.setSpeed(255);
   }
+    
+  }
+}
+
+
+void calibrate(){
+  
+  delay(500);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
+  for (int i = 0; i < 400; i++)
+  {
+    qtrrc.calibrate();
+  }
+  digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
+
+  // print the calibration minimum values measured when emitters were on
+  Serial.begin(9600);
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(qtrrc.calibratedMinimumOn[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(qtrrc.calibratedMaximumOn[i]);
+    Serial.print(' ');
+  }
+
+  
+  Serial.println();
+  Serial.println();
+  delay(1000);
+  
 }
